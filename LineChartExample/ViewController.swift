4b58afe871ffe3f830b720d5ac6bd3008c5aaff0
osianSmith <osiansmith@me.com>
2017-07-13 00:15:06 +0100

    Initial Commit

---
+++ LineChartExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LineChartExample
+//
+//  Created by Osian on 13/07/2017.
+//  Copyright © 2017 Osian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
